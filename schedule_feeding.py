7fd1140d26f80bbb73a23b420a773ab3eb475abd
rsprince15 <mmarsaudio@gmail.com>
2018-08-13 19:03:39 -0500

    Change up file naming to handle schedule feeding vs one time feeds. Added php get and set times

---
+++ schedule_feeding.py
@@ -0,0 +1,32 @@
+###############################################################
+#
+# Title: cat_feeder.py
+#
+# Function: This script is ran using crontab scheduling
+#
+#
+###############################################################
+import RPi.GPIO as GPIO
+import time
+
+# Set GPIO modes
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)
+
+# Set GPIO PWM start point
+food_container = GPIO.PWM(12, 50)
+food_container.start(7.5)
+
+###############################################################
+# Try to turn container to feed cats
+###############################################################
+try:
+    food_container.ChangeDutyCycle((60/18.0) + 2.5)  # turn towards 60 degree
+    time.sleep(1) # sleep 1 second
+    food_container.ChangeDutyCycle((0/18.0) + 2.5)  # turn towards 0 degree
+    time.sleep(1) # sleep 1 second
+    food_container.ChangeDutyCycle((60/18.0) + 2.5) # turn towards 60 degree
+except KeyboardInterrupt:
+    food_container.stop()
+
+GPIO.cleanup()
