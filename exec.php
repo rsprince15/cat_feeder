1e3cac81ce8503b93ef3a66dc639662787ba545d
Russell Prince <rsprince@mtu.edu>
2018-08-19 09:25:21 -0500

    Revert "Updated for laptop"
    This reverts commit 1bb9679d01a9365dd4ba9a272ba93bf789eb8872.

--- exec.php
+++ exec.php
@@ -1,4 +1,4 @@
 <?php
   echo "You win";
-  shell_exec('/home/evil-user/cat_feeder/feed_cats.py >> /tmp/feed_error.log 2>&1');
+  shell_exec('/home/pi/cat_feeder/feed_cats.py >> /tmp/feed_error.log 2>&1');
 ?>
