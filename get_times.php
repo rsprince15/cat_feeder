172ddd91f8fdafafe5c5cc0215a9e36ed16c49f0
rsprince15 <mmarsaudio@gmail.com>
2018-08-19 09:18:15 -0500

    Add communication between javascript and php

--- get_times.php
+++ get_times.php
@@ -1,3 +1,21 @@
 <?php
-  echo "You win";
+
+// This gets the times set in crontab
+// TODO: remove the use of sudo
+$output = shell_exec('sudo crontab -u pi -l');
+$output = preg_split("/[\s]+/", $output);
+
+//$count = 0;
+//foreach ($output as $item) {
+//	echo "$count: $item\n";
+//   $count++;
+//}
+
+try {
+   echo "$output[1]:$output[0]:";
+   echo "$output[11]:$output[10]";
+} catch (Exception $e) {
+	echo 'Caught exception: ', $e->getMessage(), "\n";
+}	
+
 ?>
